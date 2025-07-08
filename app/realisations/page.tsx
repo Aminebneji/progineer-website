0ec80bf86ed2ebe4e2cf1a637156ddc1c3a9a924
amine <aminebneji@gmail.com>
2025-07-08 17:48:33 +0200

    [dev]login and register system have to refactor and test but look ok

--- app/realisations/page.tsx
+++ app/realisations/page.tsx
@@ -1,16 +1,11 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
 import { prisma } from "@/lib/prisma";
-import Link from "next/link";
 
 export default async function Page() {
     const articles = await prisma.article.findMany();
 
     return (
         <>
-            <Button asChild>
-                <Link href="/">HOME</Link>
-            </Button>
             <div className="grid gap-6 mt-4">
                 {articles.map((article) => (
                     <Card key={article.id}>
